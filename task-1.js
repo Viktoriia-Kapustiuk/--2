02048e55c3e4eeb5f8cf44ef890ed905f25a42bc
Viktoriia-Kapustiuk <76262397+Viktoriia-Kapustiuk@users.noreply.github.com>
2021-05-22 15:15:56 +0300

    +

---
+++ task-1.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('button');
+const list = document.querySelector('list');
+const item = document.querySelector('li');
+
+btn.addEventListener('click', addItem);
+
+function addItem(e) {
+    const additItem = list.createElement('li');
+
+    li.textContent = 'This is a heading';
+}
